a685f76ae4b23883cb01865ae96686ea3810bb50
Peter Zhang <peter.zhang.henan@gmail.com>
2022-01-15 17:01:15 +0800

    add husky

--- deploy.sh
+++ deploy.sh
@@ -18,6 +18,7 @@ git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
 git push -f git@github.com:peterzhangxr/peterzhangxr.github.io.git master
+git push -f git@gitee.com:peterzhangx/peterzhangx.gitee.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
---
+++ docs/project/husky.md
@@ -0,0 +1,17 @@
+# Husky
+> Git hooks 管理工具
+## 安装
+```sh
+yarn add husky -D
+npm install husky -D
+```
+
+## 使用
+```sh
+npm set script prepare "husky install"
+npm run prepare
+```
+## 添加hook
+```
+npx husky add .husky/commit-msg "npm test"
+```
